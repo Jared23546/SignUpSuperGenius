04267d26e6d18da1cb2695de114892529520982e
Jared Christensen <90413445+Jared23546@users.noreply.github.com>
2022-03-21 15:15:51 -0600

    first commit

---
+++ SignUpSuperGenius/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SignUpSuperGenius.Models;
+
+namespace SignUpSuperGenius.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        public IActionResult Appointments()
+        {
+            return View();
+        }
+
+        
+    }
+}

4b05d05966566b596ee6b7dfd448ff6f15ca011b
Brendan Lowe <90526371+bder1999@users.noreply.github.com>
2022-03-22 17:32:31 -0600

    Add files via upload

---
+++ Models/Groups.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SignUpSuperGenius.Models
+{
+    public class Groups
+    {
+        [Key]
+        [Required]
+        public int GroupId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public ushort Size { get; set; }
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public int AppointmentId { get; set; }
+        public Appointment Appointment { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}

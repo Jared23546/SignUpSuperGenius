4621e208672b6a4d92181f9da479d2d76af72edc
trevorshumway <trevshum20@gmail.com>
2022-03-22 18:25:48 -0600

    controller

--- SignUpSuperGenius/Controllers/HomeController.cs
+++ SignUpSuperGenius/Controllers/HomeController.cs
@@ -5,37 +5,95 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-
+using SignUpSuperGenius.Models;
 
 namespace SignUpSuperGenius.Controllers
 {
     public class HomeController : Controller
     {
+        private AppointmentContext AptContext { get; set; }
 
-        public HomeController()
+        public HomeController(AppointmentContext kuso)
         {
-           
+            AptContext = kuso;
         }
 
         public IActionResult Index()
         {
+
             return View();
         }
 
         public IActionResult SignUp()
         {
-            return View();
+            var apts = AptContext.Appointments
+                .Where(x => x.Filled == false)
+                .ToList();
+            return View(apts);
         }
 
-        public IActionResult Form()
+        [HttpGet]
+        public IActionResult BookAppointment(int appointmentid)
         {
-            return View();
+            var apt = AptContext.Appointments.Single(x => x.AppointmentId == appointmentid);
+            return View(apt);
+
+            //form has to have a hidden field that is the apointment ID
+
+
         }
+
+        [HttpPost]
+        public IActionResult BookAppointment(Groups grp)
+        {
+            if (ModelState.IsValid)
+            {
+                AptContext.Add(grp);
+                AptContext.SaveChanges();
+                return RedirectToAction("Appointments");
+            }
+            else
+            {
+                return View(grp);
+            }
+        }
+
+
 
         public IActionResult Appointments()
         {
-            return View();
+            var groups = AptContext.Groups
+                .Where(x => x.Appointment.Filled == true)
+                .ToList();
+            return View(groups);
         }
 
+        [HttpGet]
+        public IActionResult Edit(int groupid)
+        {
+            var grp = AptContext.Groups.Single(x => x.GroupId == groupid);
+            return View("BookAppointment", grp);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Groups grp)
+        {
+            AptContext.Update(grp);
+            AptContext.SaveChanges();
+
+            return RedirectToAction("Appointments");
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Delete(int groupid)
+        {
+            var grp = AptContext.Groups.Single(x => x.GroupId == groupid);
+            grp.Appointment.Filled = false;
+            AptContext.Groups.Remove(grp);
+            AptContext.SaveChanges();
+            return RedirectToAction("Appointments");
+        }
     }
 }
